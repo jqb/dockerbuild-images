8a4f51af17c61b7b7154c12536c7f0c65d243272
jqb <kuba.janoszek@gmail.com>
2017-10-15 00:23:57 +0200

    setup.py: version bump 0.0.2 => 0.0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dockerbuild-images",
-    version='0.0.2',
+    version='0.0.3',
     description='Recursively builds Dockerfiles',
     author='Jakub Janoszek',
     author_email='kuba.janoszek@gmail.com',
